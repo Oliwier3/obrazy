7cb22ede6b1f2a1a259d0382ad95bffa1a7a1da5
Jocheu <88211597+Jocheu@users.noreply.github.com>
2023-04-24 12:35:52 +0200

    3 commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.view.isVisible
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val button = findViewById<Button>(R.id.przycisk)
+        val tekstview = findViewById<TextView>(R.id.tekst)
+        button.setOnClickListener{
+            findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.ciasto2)
+            tekstview.isVisible = true
+        }
     }
 }
